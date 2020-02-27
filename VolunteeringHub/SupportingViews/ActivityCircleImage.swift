0833dceb7ed472d0f1d76f63f4fd1d4e169a59a8
Juan Esteban Méndez <je.mendez@uniandes.edu.co>
2020-02-27 14:49:06 -0500

    Initial commit

---
+++ VolunteeringHub/SupportingViews/ActivityCircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  ActivityCircleImage.swift
+//  VolunteeringHub
+//
+//  Created by Juan Esteban Méndez Roys on 27/02/20.
+//  Copyright © 2020 Universidad de los Andes. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityCircleImage: View {
+    
+    var image: Image
+    
+    var body: some View {
+        image
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 10)
+    }
+}
+
+struct ActivityCircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        ActivityCircleImage(image: Image("puppie"))
+    }
+}
